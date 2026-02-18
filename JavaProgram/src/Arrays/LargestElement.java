7b0cc1cf51e2d92a4fed76e196c793abfc1dc443
Abhi110490 <65537310+Abhi110490@users.noreply.github.com>
2026-02-18 13:18:06 +0530

    Commit Largest Element Code

---
+++ JavaProgram/src/Arrays/LargestElement.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+class LargestElement {
+    public static void main(String[] args) {
+        int[] arr = {1,2,4,5,3};
+        System.out.println("Largest Element : " + largestElement(arr));
+    }
+
+    public static int largestElement(int[] arr) {
+        int max = arr[0];
+        for (int j : arr) {
+            if (max < j) {
+                max = j;
+            }
+        }
+        return max;
+    }
+}
