3ee1db7ee8c15139fc969ff57084363a6be53435
Abhi110490 <65537310+Abhi110490@users.noreply.github.com>
2026-02-18 13:21:50 +0530

    Commit new code

---
+++ JavaProgram/src/Arrays/CheckIfArrayIsSorted.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class CheckIfArrayIsSorted {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        System.out.println("Is Array Sorted : " + isSorted(arr));
+    }
+
+    private static boolean isSorted(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
